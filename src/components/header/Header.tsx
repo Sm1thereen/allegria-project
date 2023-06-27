8d1ef886118ec1bcc2e3f1242fa0c68d4c951d44
Sm1thereen <razer22833@gmail.com>
2023-06-27 10:05:28 +0300

    slider

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,77 @@
-import React from 'react';
-
+import React, {useState} from 'react';
 import './style.css'
+import {RxDotFilled} from "react-icons/rx";
 
-import header1 from '../../img/header/header-1.jpg'
-import header2 from '../../img/header/header-2.jpg'
+import americanVintage from '../../img/header/AmericanVintage.jpg';
+import georgeGinaLucy from '../../img/header/GeorgeGinaLucy.jpg';
+import deha from '../../img/header/Deha.jpg';
+import birkenstock from '../../img/header/birkenstock.jpg';
+
+
+
 const Header = () => {
+    const [currentBanner,setCurrentBanner] = useState(0);
+    const banners = [
+        {
+            image:americanVintage,
+            heading:'Brand American Vintage',
+            text:'Look collection',
+            textClass:'centered-text',
+        },
+        {
+            image:georgeGinaLucy,
+            heading:'Brand george gina\n lucy',
+            text:'Look collection',
+            textClass:'left-aligner-text',
+        },
+        {
+            image:deha,
+            heading:'DEHA',
+            text:'Look collection',
+            textClass:'left-aligner-text',
+        },
+        {
+            image:birkenstock,
+            heading:'birkenstock',
+            text:'Look collection',
+            textClass:'left-aligner-text',
+        },
+    ];
+    const changeBanner = () => {
+        const nextBanner = (currentBanner + 1) % banners.length;
+        setCurrentBanner(nextBanner)
+    }
+
+    const goToSlide = (bannerIndex) =>{
+        setCurrentBanner(bannerIndex);
+    }
+
     return (
         <div>
             <header className="header">
                 <div className="container">
                     <div className="header-wrapper">
                         <div className="header__text">
-                            <h2 className='header__title'>Brand <br/> American Vintage</h2>
-                            <p className="header__subtitle">Look collection</p>
+                            <h2 className='header__title'>{banners[currentBanner].heading}</h2>
+                            <p  className="header__subtitle">{banners[currentBanner].text}</p>
                         </div>
                         <div className="header__img">
-                            <img src={header1} alt=""/>
-                            <img src={header2} alt=""/>
+                            <img src={banners[currentBanner].image} alt=""/>
                         </div>
+                        <div className="list-wrapper">
+                            <ul className="list-number">
+                                    {banners.map((banner,bannerIndex)=> (
+                                        <li
+                                            key={bannerIndex}
+                                            onClick={() => goToSlide(bannerIndex)}
+                                            className={`dot-list ${currentBanner === bannerIndex ? 'active' : ''}`}
+                                        >
+                                            <RxDotFilled />
+                                        </li>
+                                    ))}
+                            </ul>
+                        </div>
+
                     </div>
                 </div>
             </header>
@@ -26,3 +80,4 @@ const Header = () => {
 };
 
 export default Header;
+
